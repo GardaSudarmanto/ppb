536da2370ee08de1dffffae72a5e4d3d947f83a9
Garda Sudarmanto <94687747+GardaSudarmanto@users.noreply.github.com>
2025-03-25 08:24:08 +0700

    coba1
    hehe

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:your_app_name/home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Selamat Datang di Aplikasi Belanja'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: Text('Mulai'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
